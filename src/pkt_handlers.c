470b2f207d30c48d8c1934aa32e9c8e00d32c074
Bell-Charles <qiuguo.pan@bell.ca>
2025-02-10 11:08:14 -0500

    Update pkt_handlers.c

--- src/pkt_handlers.c
+++ src/pkt_handlers.c
@@ -2066,11 +2066,12 @@ void NF_vrf_name_handler(struct channels_list_entry *chptr, struct packet_ptrs *
 {
   struct pkt_vlen_hdr_primitives *pvlen = (struct pkt_vlen_hdr_primitives *) ((*data) + chptr->extras.off_pkt_vlen_hdr_primitives);
 
-  if (check_pipe_buffer_space(chptr, pvlen, strlen(pptrs->vrf_name))) {
+  if (check_pipe_buffer_space(chptr, pvlen, strlen(pptrs->vrf_name)+1)) {
     vlen_prims_init(pvlen, 0);
     return;
   }
-  else vlen_prims_insert(pvlen, COUNT_INT_VRF_NAME, strlen(pptrs->vrf_name), (u_char *) pptrs->vrf_name, PM_MSG_STR_COPY);
+  else vlen_prims_insert(pvlen, COUNT_INT_VRF_NAME, strlen(pptrs->vrf_name)+1, (u_char *) pptrs->vrf_name, PM_MSG_STR_COPY);
+  
 }
 
 
